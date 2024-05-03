dd6fb382a1234615785197a981dd5ab880c09748
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-02 21:59:28 -0500

    shoehorned in format again

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     upper_string = ""
     for char in str:
         if 'a' <= char <= 'z':
-            upper_char = chr(ord(char) - 32)
+            upper_char = "{:}".format(chr(ord(char) - 32))
         else:
             upper_char = char
         upper_string += upper_char
