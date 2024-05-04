9a18eb6d2ebbace94015819b7c7a5ef4f483fae5
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-04 18:34:30 -0500

    added case 0

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 import sys
 if __name__ == "__main__":
     args = len(sys.argv) - 1
-    if args == 1:
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
         print("{} argument:".format(args))
     else:
         print("{} arguments:".format(args))
