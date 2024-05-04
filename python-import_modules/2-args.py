a5cc7adfa9d9b4c679187b5b8270fbbe114d6ea6
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-04 18:30:06 -0500

    added file 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+args = len(sys.argv) - 1
+if args == 1:
+    print("{} argument:".format(args))
+else:
+    print("{} arguments:".format(args))
+
+if (args > 0):
+    for i in range(1, args + 1):
+        print("{}: {}".format(i, sys.argv[i]))
