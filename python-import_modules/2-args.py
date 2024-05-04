0b9ccdc08792c9ca9935156a9f45a16ea44189a6
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-04 18:32:53 -0500

    added the name guard

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-args = len(sys.argv) - 1
-if args == 1:
-    print("{} argument:".format(args))
-else:
-    print("{} arguments:".format(args))
+if __name__ == "__main__":
+    args = len(sys.argv) - 1
+    if args == 1:
+        print("{} argument:".format(args))
+    else:
+        print("{} arguments:".format(args))
 
-if (args > 0):
-    for i in range(1, args + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    if (args > 0):
+        for i in range(1, args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
