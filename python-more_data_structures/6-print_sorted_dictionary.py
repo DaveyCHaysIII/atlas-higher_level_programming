1119e71bdd95a43bc50f15f411dc25dc44cb742e
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-05 17:33:27 -0500

    changed var name

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a_dictionary_keys = a_dictionary.keys()
-    a_dictionary_keys.sort()
-    for i in a_dictionary_keys:
+    keys = a_dictionary.keys()
+    keys.sort()
+    for i in keys:
         print(i, a_dictionary[i])
