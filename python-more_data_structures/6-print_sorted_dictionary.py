dab309910872f817989654374c947eb1fa643273
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-05 17:37:45 -0500

    tried sorted method

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    keys = a_dictionary.keys()
-    keys.sort()
-    for i in keys:
-        print(i, a_dictionary[i])
+    sorted_items = sorted(a_dictionary.items())
+    print(sorted_items)
