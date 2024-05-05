e886a33973eb8504e3141f08f3c56cfda01e5cbb
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-05 17:45:57 -0500

    rearranged logic

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sorted_items = sorted(a_dictionary.items())
-    for i in sorted_items:
-        print("{}: {}".format(i, sorted_items(i)))
+    for key, value in sorted_items:
+        print("{}: {}".format(key, value))
