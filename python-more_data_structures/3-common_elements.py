2123f4e6af74a37f831f49b0a796d6c6592aab16
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-05 17:08:03 -0500

    used intersection method

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = {}
-    for i in set_1:
-        if i in set_2:
-            common.add(i)
-    return common
+    set_common = set_1.intersection(set_2)
+    return set_common
