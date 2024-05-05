f22809957ff9fcfc8b5cfcd60f3a44ffa034c28d
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-05 17:42:12 -0500

    trying print format

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sorted_items = sorted(a_dictionary.items())
-    print(sorted_items)
+    for i in sorted_items:
+        print("{}: {}".format(i, sorted_items(i)))
