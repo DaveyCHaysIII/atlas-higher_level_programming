d16f1d3474a32562eaae16a59fd0b9926d2d2d2f
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-05 18:15:22 -0500

    changed logic

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
-    for key, value in new_dict:
-        new_dict.update({key: value * 2})
+    for i in new_dict:
+        new_dict[i] *= 2
     return new_dict
