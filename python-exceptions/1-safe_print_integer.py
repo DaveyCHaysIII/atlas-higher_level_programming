2e167cb5e81b520ec9b756c4f27b89791ec04f47
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-07 19:39:16 -0500

    added file 1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        val_printed = True
+        print("{:d}.format()")
+    except TypeError:
+        val_printed = False
+    finally:
+        return val_printed
