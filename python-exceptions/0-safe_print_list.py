474e118c010431b0f26f2977dd637915fb0207d9
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-07 19:33:45 -0500

    re-read problem and re-wrote solution

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        count = 0
         for i in range(x):
-            print(my_list[i], end='\n')
+            print(my_list[i], end='')
+            count += 1
     except IndexError:
-        print("Outside of range!")
+        pass
+    finally:
+        print()
+        return count
