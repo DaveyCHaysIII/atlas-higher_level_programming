900762475ef42cc4afd1632fd86301bb39cd3197
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-12 11:01:54 -0500

    added file 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Trying out validation on class creation"""
+
+
+class Square:
+    """class with attr size that MUST be 0"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """getter for size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Method to return area of square"""
+
+        return self.__size * self.__size
+
+    def my_print(self):
+        """function to print square"""
+
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
