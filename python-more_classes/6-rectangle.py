7ac96b724dfcca4267f420baf4443bd114f43d84
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-12 15:24:42 -0500

    changed public attribute

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,7 +10,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    number_of_instances += 1
+
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -75,4 +76,4 @@ class Rectangle:
     def __del__(self):
         """only hits upon deletion"""
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
