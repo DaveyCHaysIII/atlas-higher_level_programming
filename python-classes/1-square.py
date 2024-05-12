f80a3fa8e865fc6027548ad44ec7579c08795d21
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-12 10:07:15 -0500

    added file 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define a class with private attribute _size"""
+
+
+class Square:
+    """private attribute size"""
+
+    def __init__(self, _size):
+        self._size = _size
