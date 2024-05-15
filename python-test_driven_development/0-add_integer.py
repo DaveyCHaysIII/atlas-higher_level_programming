8daae27eaae048473d0f726bf4ac7d27aea1516e
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-14 20:57:40 -0500

    changed text on module doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Module for adding two integers a and b together"""
+
+
+def add_integer(a, b=98):
+    """adds a + b
+
+        a = int/float
+        b = int/float, def int 98
+
+        Returns: sum a + b
+    """
+    return a + b
