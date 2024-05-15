ccc1e78cfc26b23f91d5538b9441073d8391d015
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-14 21:02:57 -0500

    added raises

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,4 +10,9 @@ def add_integer(a, b=98):
 
         Returns: sum a + b
     """
-    return a + b
+    if not isinstance(a, int) or not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) or not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
