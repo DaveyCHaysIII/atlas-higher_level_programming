463dc64b230d56fd7106bdffd456c9a0f77b4504
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-19 15:00:02 -0500

    added file 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class Rectangle based on BaseGeometry"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class to describe Rectangles
+
+    private args:
+    width: integer
+    height: integer
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
