bc41dcca95a30880adc961109501b5b9ba63031e
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-19 15:11:33 -0500

    self

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -15,9 +15,11 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
-    def __str__(self, width, height):
-        return ("[Rectangle]: {}/{}".format(width, height, end=""))
+    def __str__(self):
+        return ("[Rectangle]: {}/{}".format(self.__width, self.__height))
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
