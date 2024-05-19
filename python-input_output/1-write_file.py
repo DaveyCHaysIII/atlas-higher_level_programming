efedb4d87b0648b34bf6a376c8d9d1ec90813cf7
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-19 15:59:36 -0500

    added file 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a function that writes a string (UTF-8) to file"""
+
+def write_file(filename="", text=""):
+    """
+    Write text to file
+
+    Args:
+    filename- file in question
+    text- text in question
+
+    Returns: nothing
+    """
+    with open(filename, 'w') as f:
+        f.write(text)
