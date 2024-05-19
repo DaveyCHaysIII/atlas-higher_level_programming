ce1cf853908c8b0c21dcfa61e622769bec1c5ab4
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-19 13:11:13 -0500

    added file 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Create a base class base_geometry"""
+
+
+class BaseGeometry():
+    """
+    Create an empty BaseGeometry class
+
+    Args:
+    none
+
+    Returns:
+    none"""
+    pass
