355157a534ce5ae4c66e94f8e64865dd2c4edb11
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-19 19:58:34 -0500

    removed os module

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """write json to file"""
 import json
-import os
 
 
 def save_to_json_file(obj, filename):
@@ -11,6 +10,5 @@ def save_to_json_file(obj, filename):
     obj- the object to write
     Returns- no return"""
     dat = json.dumps(obj)
-    if os.access(filename, os.W_OK):
-        with open(filename, mode="w", encoding="utf-8") as f:
-            f.write(dat)
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(dat)
