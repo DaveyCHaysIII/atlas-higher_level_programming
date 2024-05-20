4ceade50312dca682b8a1846e88c681102935951
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-19 19:37:11 -0500

    docd func

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,6 +4,11 @@ import json
 
 
 def save_to_json_file(obj, filename):
+    """write json to file
+    Args:
+    filename- file in question
+    obj- the object to write
+    Returns- no return"""
     with open(filename, mode="w", encoding="utf-8") as f:
         dat = json.dumps(obj)
         f.write(dat)
