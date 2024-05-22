399a9b1959c9cc5280850be6acb8c6383cfa1632
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-21 21:27:50 -0500

    reverted change

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
-    arguments = sys.argv
+    arguments = sys.argv[1:]
 
     try:
         my_list = load_from_json_file("add_item.json")
