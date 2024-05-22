3557443623eb49f1de8f7b3e1da540fca4a28221
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-21 21:14:47 -0500

    added file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-"""test"""
-import json
+"""Add command line arguments to list, save"""
 import sys
 
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+arguments = sys.argv[1:]
+
+try:
+    my_list = load_from_json_file("add_items.json")
+except FileNotFoundError:
+    my_list = []
+for arg in arguments:
+    my_list.append(arg)
+
+save_to_json_file(my_list, "add_items.json")
