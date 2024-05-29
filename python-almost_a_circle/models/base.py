175f8f9ce8a419532a69e82f174d2d217af92f93
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-28 21:32:59 -0500

    pycode style

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,5 +40,5 @@ class Base:
         """Returns list from json_string"""
         if json_string is None or len(json_string) == 0:
             return []
-        else: 
+        else:
             json.loads(json_string)
