c8e9639462c2cb645d124b3d00514b10bb6bc9ea
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-28 21:21:11 -0500

    small add to rect setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -57,7 +57,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         else:
             self.__x = value
 
@@ -72,7 +72,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         else:
             self.__y = value
 
