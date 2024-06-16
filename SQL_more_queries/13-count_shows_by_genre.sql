8683017c73c4701588083341fbe5a86bb09f6e3b
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-16 13:59:10 -0500

    finished all files in proj 2

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+--write a script that displays all genres and the number of shows they contain
+SELECT DISTINCT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
