1eab036c83b92e2f6e473a538f2025d48785a0d2
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-18 17:16:13 -0500

    reverted change to file 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -29,7 +29,7 @@ def list_states(username, password, database, statename):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print("Usage: ./script.py"
               + " <mysql username> <mysql password> <database name>")
         sys.exit(1)
