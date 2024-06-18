ddd87fa2bd78f9583a3ea94d7b5db5587e4e1cb4
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-17 21:57:32 -0500

    corrections to more queries

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- write a script that creates a database with a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES (state_id),
-    name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS 'cities' (
+    'id' INT AUTO_INCREMENT NOT NULL,
+    'state_id' INT NOT NULL,
+    'name' VARCHAR(256) NOT NULL,
+    PRIMARY KEY ('id'),
+    FOREIGN KEY ('state_id') REFERENCES ('state_id'),
     );
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- write a script that lists all cities in database
-SELECT id, name, states.name 
-FROM cities
-LEFT JOIN states ON cities.state_id = state_id
-ORDER BY cities.id ASC;
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities` 
+LEFT JOIN 'states' ON 'states'.'id' = 'cities'.'state_id'
+ORDER BY 'cities'.'id';
