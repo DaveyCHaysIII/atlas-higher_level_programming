3c9031a7f9b98a55cad0aa8facc11c7728c2a8b0
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-18 18:00:31 -0500

    added fixes to file 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,33 +1,48 @@
 #!/usr/bin/python3
-"""write a script that lists all cities from passed states from hbtn_0e_0_usa"""
+"""write a script that lists all cities from hbtn_0e_4_usa"""
 
 import sys
 import MySQLdb
 
+
 def list_states(username, password, database, statename):
 
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+    db = MySQLdb.connect(
+         host="localhost",
+         user=username,
+         passwd=password,
+         db=database,
+         port=3306
+         )
 
-    cursor = db.cursor();
+    cursor = db.cursor()
 
-    cursor.execute("SELECT cities.name FROM cities JOIN states ON states.id = cities.state_id WHERE states.name = '{}'".format(statename))
+    query = ("SELECT cities.name "
+             + "FROM states INNER JOIN cities "
+             + "ON cities.state_id = states.id "
+             + "WHERE states.name = %s"
+             + "ORDER BY cities.id")
+
+    cursor.execute(query, (statename,))
 
     rows = cursor.fetchall()
 
     for row in rows:
-        print (row)
+        print(row)
 
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-            print("Usage: ./script.py <mysql username> <mysql password> <database name>")
-            sys.exit(1)
+        print("Usage: ./script.py " +
+              "<mysql username> <mysql password> <database name>")
+        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    statename= sys.argv[4]
+    statename = sys.argv[4]
 
     list_states(username, password, database, statename)
