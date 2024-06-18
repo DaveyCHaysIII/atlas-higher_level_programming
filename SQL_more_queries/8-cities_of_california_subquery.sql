33cef675c1c432e0ddee73373314eec18d2a7b6a
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-17 22:03:17 -0500

    small change to file 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- write a script that lists all the cities in california in a database
-SELECT 'id', 'name' FROM 'cities' WHERE 'state_id' = (
-    SELECT 'id' FROM 'states' WHERE 'name' = 'California' 
-) ORDER BY 'id';
+SELECT `id`, `name` FROM `cities` 
+WHERE `state_id` = (SELECT `id` FROM `states` WHERE `name` = 'California') 
+ORDER BY `id`;
