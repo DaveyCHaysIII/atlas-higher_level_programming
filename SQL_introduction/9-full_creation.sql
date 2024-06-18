ee5b6650ea5de2886dc6f8ed25b048220e9cb535
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-17 21:32:08 -0500

    corrected question 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- this script genereates a populated table for a database
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
     VALUES (1, 'John', 10),
     (2, 'Alex', 3),
