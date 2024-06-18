6dee629e52bd90fae77302ebb8547dcb34efbfbc
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-17 22:06:26 -0500

    corrected file 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- write a script that lists all cities in database
 SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities` 
-LEFT JOIN 'states' ON 'states'.'id' = 'cities'.'state_id'
-ORDER BY 'cities'.'id';
+LEFT JOIN `states` ON `states`.`id` = `cities`.`state_id`
+ORDER BY `cities`.`id`;
