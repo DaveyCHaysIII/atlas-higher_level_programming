fc42a49d6f5154a043d7dd386b14cc65d53eac89
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-06-18 19:09:06 -0500

    added file 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""write a script that lists all state objs from hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states(username, password, db):
+    mysql_local = ("mysql://{}:{}@localhost:3306/{}")
+    engine = create_engine(mysql_local.format(username, password, db))
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    username, password, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, db)
