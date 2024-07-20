080a377abcedac344c744bfd81a52bad01ee01b5
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-20 21:59:53 +0000

    reworked to fit checker

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-    } else {
+    if (w <= 0) {
       this.width = w;
+    }
+    if (h <= 0) {
       this.height = h;
     }
   }
