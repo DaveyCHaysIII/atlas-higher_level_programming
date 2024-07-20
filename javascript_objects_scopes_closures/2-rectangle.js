0359fcc56455d72c7dbef52e39aa9fc3782e6389
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-20 22:01:46 +0000

    wrong operators

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0) {
+    if (w >= 0) {
       this.width = w;
     }
-    if (h <= 0) {
+    if (h >= 0) {
       this.height = h;
     }
   }
