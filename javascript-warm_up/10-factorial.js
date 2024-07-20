3cee091c051579ab1cc85bc05b31435813cee5b7
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-20 04:10:40 +0000

    added file 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const argv = process.argv;
+const num = Number(argv[2]);
+
+function factorial (n) {
+  if (n === 0 || n === 1 || isNaN(num)) {
+    return (1);
+  } else {
+    return (n * factorial(n - 1));
+  }
+}
+
+console.log(factorial(num));
