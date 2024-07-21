78170dc2cf864170f75244f854e5d181f1b0ced4
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-21 03:34:36 +0000

    added file 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,52 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(`${c}`);
+        }
+        process.stdout.write('\n');
+      }
+    }
+  }
+
+  rotate () {
+    const height = this.height;
+    const width = this.width;
+    this.height = width;
+    this.width = height;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
