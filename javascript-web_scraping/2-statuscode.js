8fcf4587fa12a29899c4154dd2cf348c753e602c
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-26 02:52:50 +0000

    added file 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request(`${url}`, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
