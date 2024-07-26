fb343e3f8ec15b121d19458454fcf57a2c67c88d
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-26 02:40:29 +0000

    semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,9 +4,8 @@ const path = process.argv[2];
 const data = process.argv[3];
 
 const fs = require('node:fs');
-fs.writeFile(`${path}`, `${data}`,'utf-8', (err) => {
+fs.writeFile(`${path}`, `${data}`, 'utf-8', (err) => {
   if (err) {
     console.log(err);
-    return;
   }
 });
